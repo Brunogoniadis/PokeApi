74ce09d9e7b88651a48f8d5b5ae00e9707d72f83
Bruno Goniadis <brunogoniadislima@gmail.com>
2022-11-22 23:23:15 -0300

    implement: PrincipalContainers/Buttons with Styled Comp

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const  AppContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+` 
+
+export const PokemonContainer = styled.div`
+`
+export const Allcontainer = styled.div`    
+`
+export const LoadMore = styled.button`
+
+`    
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import  { AppContainer }  from "./App.js"
+import { PokemonContainer } from "./App.js"
+import { Allcontainer } from "./App.js"
+import { LoadMore } from "./App.js"
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <AppContainer>
+      <h1>Pokemon Evolution</h1>
+      <PokemonContainer>
+        <Allcontainer> 
+
+        </Allcontainer>
+        <LoadMore></LoadMore>
+      </PokemonContainer>
+    </AppContainer>
   )
 }
 
-export default App
+export default App;
